1583ca38c48160a10a9d7c67e62d746cbd9fad4d
nazim <nazimmalyshev47@gmail.com>
2020-07-15 19:16:10 +0300

    15.07.2020

---
+++ dataStruct/seg_tree.cpp
@@ -0,0 +1,42 @@
+#include<vector>
+#include <string>
+#include<algorithm>
+#include <iostream>
+#include <queue>
+#include<set>
+#include<stack>
+#include<cmath>
+#include<math.h>
+#include<map>
+#include<unordered_map>
+using namespace std;
+typedef long long ll;
+typedef pair<long long, long long> pll;
+#define mp make_pair
+#define fi(b, c) for(auto i = b; i <= c; i++)
+#define fj(b, c) for(auto j = b; j <= c; j++)
+#define fk(b, c) for(auto k = b; k <= c; k++)
+#define fim(b, c) for(auto i = b; i >= c; i--)
+#define fjm(b, c) for(auto j = b; j >= c; j--)
+#define fkm(b, c) for(auto k = b; k >= c; k--)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(a) (ll)(a.size())
+#define fs first
+#define sd second
+#define endl "\n"
+#define cin(a) for(ll o = 0; o<a.size(); o++) cin>>a[o];
+#define cout(a) for(ll o = 0; o<a.size(); o++) cout<<a[o]<<" ";
+const ll inf = 1e9 + 123, llinf = 1e18 + 123;
+void xru(){
+ setlocale(LC_ALL, "rus");
+ /*freopen(".in", "r", stdin);
+ freopen(".out", "w", stdout);*/
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+ cout.tie(NULL);
+}
+void run(){
+ cout<<endl;
+ system("pause");
+}
